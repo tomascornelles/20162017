c0568e9f5f0c089dca1fff75f8b26ca12a31c4bb
Tomas Cornelles <tomascornelles@gmail.com>
2016-02-16 21:48:19 +0100

    vstudio

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,25 +1,45 @@
-$(function(){
-	
-	historia.scrolling();
+var $
 
-});
+$(function () {
+  historia.scrolling()
+})
 
 var historia = {
-	scrolling: function() {
-		var scrollTemp = $('body').scrollTop();
-		var paginaTemp = Math.floor($('body').scrollTop() / $(window).height());
+  scrolling: function () {
+    var scrollTemp = $('body').scrollTop()
+    var paginaTemp = Math.floor($('body').scrollTop() / $(window).height())
 
-		$(window).on('scroll', function() {
-			var scrollNew = $('body').scrollTop();
-			if (scrollNew > scrollTemp) {
-				paginaTemp += 1;
-				$('body').scrollTop($(window).height() * paginaTemp);
-				scrollTemp = $('body').scrollTop();
-			} else if (scrollNew < scrollTemp) {
-				paginaTemp -= 1;
-				$('body').scrollTop($(window).height() * paginaTemp);
-				scrollTemp = $('body').scrollTop();
-			}
-		});
-	}
+    $(window)
+      .on('scrollstart', function () {
+        var scrollNew = $('body').scrollTop()
+
+        if (scrollNew > scrollTemp) {
+          scrollNew = undefined
+          paginaTemp += 1
+
+          $('body').animate({
+            scrollTop: $(window).height() * paginaTemp },
+            '500',
+            'swing',
+            function () {
+              scrollTemp = $('body').scrollTop()
+            })
+        } else if (scrollNew < scrollTemp) {
+          scrollNew = undefined
+          paginaTemp -= 1
+
+          $('body').animate({
+            scrollTop: $(window).height() * paginaTemp },
+            '500',
+            'swing',
+            function () {
+              if (paginaTemp < 0) paginaTemp = 0
+              scrollTemp = $('body').scrollTop()
+            })
+        }
+      })
+      .on('scrollstop', function () {
+        console.log('termina Scroll')
+      })
+  }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var nano = require('gulp-cssnano');
+var gulp = require('gulp')
+var webserver = require('gulp-webserver')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var sass = require('gulp-sass')
+var nano = require('gulp-cssnano')
 
-gulp.task('templates', function() {
-	gulp.src('./app/views/*.html')
-	.pipe(gulp.dest('./dist/'));
-});
+gulp.task('templates', function () {
+  gulp.src('./app/views/*.html')
+  .pipe(gulp.dest('./dist/'))
+})
 
 gulp.task('sassmin', function () {
-	gulp.src('./app/sass/*.sass')
-	.pipe(sass())
-	.pipe(concat('styles.css'))
-	.pipe(nano())
-	.pipe(gulp.dest('./dist/'));
-});
+  gulp.src('./app/sass/*.sass')
+  .pipe(sass())
+  .pipe(concat('styles.css'))
+  .pipe(nano())
+  .pipe(gulp.dest('./dist/'))
+})
 
 gulp.task('cssmin', function () {
-	gulp.src('./app/css/*.css')
-	.pipe(concat('libs.css'))
-	.pipe(gulp.dest('./dist/'));
-});
+  gulp.src('./app/css/*.css')
+  .pipe(concat('libs.css'))
+  .pipe(gulp.dest('./dist/'))
+})
 
 gulp.task('jsmin', function () {
-	gulp.src('./app/js/*.js')
-	.pipe(concat('scripts.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('./dist/'));
+  gulp.src('./app/js/*.js')
+  .pipe(concat('scripts.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/'))
 
-	gulp.src('./app/js/libs/*.js')
-	.pipe(concat('libs.js'))
-	.pipe(gulp.dest('./dist/'));
-});
+  gulp.src('./app/js/libs/*.js')
+  .pipe(concat('libs.js'))
+  .pipe(gulp.dest('./dist/'))
+})
 
-gulp.task('server', function() {
-	gulp.src('./dist/')
-	.pipe(webserver({
-		livereload: true,
-		open: true,
-		port: 3000
-	}));
-});
+gulp.task('server', function () {
+  gulp.src('./dist/')
+  .pipe(webserver({
+    livereload: true,
+    open: true,
+    port: 3000
+  }))
+})
 
-gulp.task('watch', function() {
-	gulp.watch('./app/js/*.js', ['jsmin']);
-	gulp.watch('./app/sass/*.sass', ['sassmin']);
-	gulp.watch('./app/css/*.css', ['cssmin']);
-	gulp.watch('./app/views/*.html', ['templates']);
-});
+gulp.task('watch', function () {
+  gulp.watch('./app/js/*.js', ['jsmin'])
+  gulp.watch('./app/sass/*.sass', ['sassmin'])
+  gulp.watch('./app/css/*.css', ['cssmin'])
+  gulp.watch('./app/views/*.html', ['templates'])
+})
 
-gulp.task('default', ['templates', 'sassmin', 'cssmin', 'jsmin', 'server', 'watch']);
+gulp.task('default', ['templates', 'sassmin', 'cssmin', 'jsmin', 'server', 'watch'])
