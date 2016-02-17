b82affad07954347d2335182821338f792f8a81b
tomascornelles <menos5@gmail.com>
2016-02-17 11:52:44 +0100

    without standard js

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,45 +1,45 @@
-var $
+var $;
 
 $(function () {
-  historia.scrolling()
-})
+  historia.scrolling();
+});
 
 var historia = {
   scrolling: function () {
-    var scrollTemp = $('body').scrollTop()
-    var paginaTemp = Math.floor($('body').scrollTop() / $(window).height())
+    var scrollTemp = $('body').scrollTop();
+    var paginaTemp = Math.floor($('body').scrollTop() / $(window).height());
 
     $(window)
       .on('scrollstart', function () {
-        var scrollNew = $('body').scrollTop()
+        var scrollNew = $('body').scrollTop();
 
         if (scrollNew > scrollTemp) {
-          scrollNew = undefined
-          paginaTemp += 1
+          scrollNew = undefined;
+          paginaTemp += 1;
 
           $('body').animate({
             scrollTop: $(window).height() * paginaTemp },
             '500',
             'swing',
             function () {
-              scrollTemp = $('body').scrollTop()
-            })
+              scrollTemp = $('body').scrollTop();
+            });
         } else if (scrollNew < scrollTemp) {
-          scrollNew = undefined
-          paginaTemp -= 1
+          scrollNew = undefined;
+          paginaTemp -= 1;
 
           $('body').animate({
             scrollTop: $(window).height() * paginaTemp },
             '500',
             'swing',
             function () {
-              if (paginaTemp < 0) paginaTemp = 0
-              scrollTemp = $('body').scrollTop()
-            })
+              if (paginaTemp < 0) paginaTemp = 0;
+              scrollTemp = $('body').scrollTop();
+            });
         }
       })
       .on('scrollstop', function () {
-        console.log('termina Scroll')
-      })
+        console.log('termina Scroll');
+      });
   }
-}
+};
