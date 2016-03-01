6432c3a86fe56cced14a190028c41f2eb1a660ce
Tomas Cornelles <tomascornelles@gmail.com>
2016-03-01 08:21:51 +0100

    updates scroll

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,49 +1,47 @@
-var $;
+/* global $ */
 
-$(function() {
-  historia.scrolling();
-});
+$(function () {
+  historia.scrolling()
+})
 
 var historia = {
-  scrolling: function() {
-    var scrollTemp = $('body').scrollTop();
-    var paginaTemp = Math.floor($('body').scrollTop() / $(window).height());
+  scrolling: function () {
+    var scrollTemp = $('body').scrollTop()
+    var paginaTemp = Math.floor($('body').scrollTop() / $(window).height())
 
     $(window)
-      .on('scrollstart', function() {
-      var scrollNew = $('body').scrollTop();
+      .on('scrollstart', function () {
+        var scrollNew = $('body').scrollTop()
 
-      if (scrollNew > scrollTemp) {
-        scrollNew = undefined;
-        paginaTemp += 1;
+        if (scrollNew > scrollTemp) {
+          scrollNew = undefined
+          paginaTemp += 1
 
-        $('body').animate({
-          scrollTop: $(window).height() * paginaTemp
-        },
+          $('body').animate({
+            scrollTop: $(window).height() * paginaTemp
+          },
           '500',
           'swing',
+          function () {
+            scrollTemp = $('body').scrollTop()
+          })
+        } else if (scrollNew < scrollTemp) {
+          scrollNew = undefined
+          paginaTemp -= 1
 
-        function() {
-          scrollTemp = $('body').scrollTop();
-        });
-      } else if (scrollNew < scrollTemp) {
-        scrollNew = undefined;
-        paginaTemp -= 1;
-
-        $('body').animate({
-          scrollTop: $(window).height() * paginaTemp
-        },
+          $('body').animate({
+            scrollTop: $(window).height() * paginaTemp
+          },
           '500',
           'swing',
-
-        function() {
-          if (paginaTemp < 0) paginaTemp = 0;
-          scrollTemp = $('body').scrollTop();
-        });
-      }
-    })
-      .on('scrollstop', function() {
-      console.log('termina Scroll');
-    });
+          function () {
+            if (paginaTemp < 0) paginaTemp = 0
+            scrollTemp = $('body').scrollTop()
+          })
+        }
+      })
+      .on('scrollstop', function () {
+        console.log('termina Scroll')
+      })
   }
-};
+}
